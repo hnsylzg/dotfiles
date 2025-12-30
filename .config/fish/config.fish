ca6350452aaffd530a981cb6fffe99e6b8157850
hnsylzg <hnsylzg@gmail.com>
2025-12-30 23:37:38 +0800

    commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,40 +1,30 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    # Aliases
-    if [ -f $HOME/.config/fish/aliases.fish ]
-        source $HOME/.config/fish/aliases.fish
-    end
-
-    # Enable the fish greeting
-    set -U fish_greeting ""
-
-    # Enable neofetch or the custom bash script for getting images in neofetch
-    #source ~/.config/neofetch/launch-neofetch.sh
-    #neofetch
-    #fastfetch
-
-    # Enable yay autocomplete in the fish shell
-    complete -c yay -f -n __fish_yay_search_packages
-    function __fish_yay_search_packages
-        eval yay -Ssq $argv
-    end
-
-    ## Environment setup
+    # Environment setup
     # Apply .profile: use this to put fish compatible .profile stuff in
     if test -f ~/.fish_profile
         source ~/.fish_profile
     end
-
+    #
     # Add ~/.local/bin to PATH
-    if test -d ~/.local/bin
-        if not contains -- ~/.local/bin $PATH
-            set -p PATH ~/.local/bin
-        end
+    fish_add_path ~/.local/bin
+
+    # Enable the fish greeting
+    set -g fish_greeting ""
+
+    # Aliases
+    if test -f ~/.config/fish/aliases.fish
+        source ~/.config/fish/aliases.fish
     end
+
+    # Enable fastfetch
+    #fastfetch
 
     # The line below is needed to make starship working in the fish shell
     starship init fish | source
 end
 
-set -gx EDITOR nvim
+if not contains /usr/share/fish/vendor_completions.d $fish_complete_path
+    set -p fish_complete_path /usr/share/fish/vendor_completions.d
+end
