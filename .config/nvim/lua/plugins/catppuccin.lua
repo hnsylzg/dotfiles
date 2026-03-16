f2b8cd264c4a7842a69f5c6a53b17d455743c801
hnsylzg <hnsylzg@gmail.com>
2026-03-16 18:36:35 +0800

    commit

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			transparent_background = true,
-		},
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
 }
